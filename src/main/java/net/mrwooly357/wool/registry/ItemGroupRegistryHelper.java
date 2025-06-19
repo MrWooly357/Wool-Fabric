22f0306030224a503ec896495695882a7839d402
MrWooly125 <belov.20.04@mail.ru>
2025-06-19 22:25:12 +0300

    ItemGroupRegistryHelper added

---
+++ src/main/java/net/mrwooly357/wool/registry/ItemGroupRegistryHelper.java
@@ -0,0 +1,23 @@
+package net.mrwooly357.wool.registry;
+
+import net.minecraft.item.ItemGroup;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+/**
+ * A helper used for registering custom {@link ItemGroup}s.
+ */
+public interface ItemGroupRegistryHelper {
+
+
+    /**
+     * Registers a custom {@link ItemGroup}.
+     * @param id the {@link Identifier}.
+     * @param itemGroup the {@link ItemGroup}.
+     * @return a registered {@link ItemGroup}.
+     */
+    static ItemGroup register(Identifier id, ItemGroup itemGroup) {
+        return Registry.register(Registries.ITEM_GROUP, id, itemGroup);
+    }
+}
