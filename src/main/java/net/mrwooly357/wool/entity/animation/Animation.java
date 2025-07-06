1d429f330e9f371d4f6079f9bb1de499c7ce824d
MrWooly125 <belov.20.04@mail.ru>
2025-07-06 14:49:34 +0300

    Bug fix

--- src/main/java/net/mrwooly357/wool/entity/animation/Animation.java
+++ src/main/java/net/mrwooly357/wool/entity/animation/Animation.java
@@ -115,7 +115,7 @@ public record Animation(Identifier entityType, Identifier actionId, boolean loop
         private Variant currentVariant;
         private int elapsedTicks;
 
-        public Player(Entity entity, EntityModel<? extends Animatable.Server> model) {
+        public Player(Entity entity, EntityModel<? extends Animatable.Client.Model> model) {
             if (entity instanceof Animatable.Server server)
                 serverAnimatable = server;
 
