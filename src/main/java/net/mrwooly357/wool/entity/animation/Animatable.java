1130ab140c8b46dbe42f036d30b7e8b0b5ac2ad5
MrWooly125 <belov.20.04@mail.ru>
2025-07-11 20:41:27 +0300

    Another small update

--- src/main/java/net/mrwooly357/wool/entity/animation/Animatable.java
+++ src/main/java/net/mrwooly357/wool/entity/animation/Animatable.java
@@ -93,6 +93,15 @@ public interface Animatable {
 
                         if (part != null) {
                             Animation.Transformation transformation = bone.getValue();
+                            System.out.println(transformation.x() + " 1");
+                            System.out.println(-transformation.y() + " 1");
+                            System.out.println(transformation.z() + " 1");
+                            System.out.println(transformation.pitch() + " 1");
+                            System.out.println(transformation.yaw() + " 1");
+                            System.out.println(transformation.roll() + " 1");
+                            System.out.println(transformation.xScale() + 1.0F + " 1");
+                            System.out.println(transformation.yScale() + 1.0F + " 1");
+                            System.out.println(transformation.zScale() + 1.0F + " 1");
                             part.pivotX = transformation.x();
                             part.pivotY = -transformation.y();
                             part.pivotZ = transformation.z();
@@ -102,6 +111,15 @@ public interface Animatable {
                             part.xScale = transformation.xScale() + 1.0F;
                             part.yScale = transformation.yScale() + 1.0F;
                             part.zScale = transformation.zScale() + 1.0F;
+                            System.out.println(transformation.x() + " 2");
+                            System.out.println(-transformation.y() + " 2");
+                            System.out.println(transformation.z() + " 2");
+                            System.out.println(transformation.pitch() + " 2");
+                            System.out.println(transformation.yaw() + " 2");
+                            System.out.println(transformation.roll() + " 2");
+                            System.out.println(transformation.xScale() + 1.0F + " 2");
+                            System.out.println(transformation.yScale() + 1.0F + " 2");
+                            System.out.println(transformation.zScale() + 1.0F + " 2");
                         }
                     }
                 }
