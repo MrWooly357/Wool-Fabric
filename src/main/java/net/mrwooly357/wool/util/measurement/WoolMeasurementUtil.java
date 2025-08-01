7a593327241f965c4da4f4ceebce2ed67828e429
MrWooly125 <belov.20.04@mail.ru>
2025-08-01 14:27:39 +0300

    Misc stuff

---
+++ src/main/java/net/mrwooly357/wool/util/measurement/WoolMeasurementUtil.java
@@ -0,0 +1,9 @@
+package net.mrwooly357.wool.util.measurement;
+
+public final class WoolMeasurementUtil {
+
+
+    public static int toInt(float value, int multiplier) {
+        return (int) (value * multiplier);
+    }
+}
