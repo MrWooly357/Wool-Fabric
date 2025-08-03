83b0177caa827c41af262bd18f74a4603885b191
MrWooly125 <belov.20.04@mail.ru>
2025-08-03 14:12:48 +0300

    Bug fix

--- src/main/java/net/mrwooly357/wool/block_util/ExtendedBlockWithEntity.java
+++ src/main/java/net/mrwooly357/wool/block_util/ExtendedBlockWithEntity.java
@@ -10,7 +10,6 @@ import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.mrwooly357.wool.block_util.entity.ExtendedBlockEntity;
-import net.mrwooly357.wool.block_util.entity.TickableBlockEntity;
 import org.jetbrains.annotations.Nullable;
 
 public abstract class ExtendedBlockWithEntity extends BlockWithEntity {
@@ -27,22 +26,22 @@ public abstract class ExtendedBlockWithEntity extends BlockWithEntity {
         return createExtendedBlockEntity(pos, state);
     }
 
+    protected abstract BlockEntityType<? extends ExtendedBlockEntity> getExpectedBlockEntityType();
+
     @Override
     public final @Nullable <T extends BlockEntity> BlockEntityTicker<T> getTicker(World world, BlockState state, BlockEntityType<T> type) {
-        return (world1, pos, state1, blockEntity) -> {
+        return validateTicker(type, getExpectedBlockEntityType(), (world1, pos, state1, blockEntity) -> {
             if (!world1.isClient()) {
-                TickableBlockEntity.Server serverTickable = (TickableBlockEntity.Server) blockEntity;
                 ServerWorld serverWorld = (ServerWorld) world1;
 
-                if (serverTickable.canTickServer(serverWorld, pos, state1))
-                    serverTickable.serverTick(serverWorld, pos, state1);
+                if (blockEntity.canTickServer(serverWorld, pos, state1))
+                    blockEntity.serverTick(serverWorld, pos, state1);
             } else {
-                TickableBlockEntity.Client clientTickable = (TickableBlockEntity.Client) blockEntity;
                 ClientWorld clientWorld = (ClientWorld) world1;
 
-                if (clientTickable.canTickClient(clientWorld, pos, state1))
-                    clientTickable.clientTick(clientWorld, pos, state1);
+                if (blockEntity.canTickClient(clientWorld, pos, state1))
+                    blockEntity.clientTick(clientWorld, pos, state1);
             }
-        };
+        });
     }
 }
