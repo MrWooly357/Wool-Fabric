5abdf17094bb15e4b769093df150b76665cd4ce5
MrWooly125 <belov.20.04@mail.ru>
2025-08-13 13:59:35 +0300

    Additional misc changes

--- src/main/java/net/mrwooly357/wool/custom_biome/region/VanillaRegionTypes.java
+++ src/main/java/net/mrwooly357/wool/custom_biome/region/VanillaRegionTypes.java
@@ -1,29 +1,22 @@
 package net.mrwooly357.wool.custom_biome.region;
 
+import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 import net.mrwooly357.wool.Wool;
-import net.mrwooly357.wool.config.custom.WoolConfig;
-import net.mrwooly357.wool.registry.helper.RegionTypeRegistryHelper;
+import net.mrwooly357.wool.registry.WoolRegistries;
 
 public final class VanillaRegionTypes {
 
-    public static final RegionType OVERWORLD = register(
-            "overworld", new RegionType()
-    );
-    public static final RegionType THE_NETHER = register(
-            "the_nether", new RegionType()
-    );
-    public static final RegionType THE_END = register(
-            "the_end", new RegionType()
-    );
+    public static final RegionType OVERWORLD = register("overworld", new RegionType());
+    public static final RegionType THE_NETHER = register("the_nether", new RegionType());
+    public static final RegionType THE_END = register("the_end", new RegionType());
 
 
     private static RegionType register(String name, RegionType regionType) {
-        return RegionTypeRegistryHelper.register(Identifier.ofVanilla(name), regionType);
+        return Registry.register(WoolRegistries.REGION_TYPE, Identifier.ofVanilla(name), regionType);
     }
 
     public static void initialize() {
-        if (WoolConfig.enableDeveloperMode)
-            Wool.LOGGER.info("Initializing vanilla region types");
+            Wool.logInitializing("Initializing vanilla region types");
     }
 }
